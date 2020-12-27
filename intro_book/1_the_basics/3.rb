71d7ee4c1d177055f5a89e524979d2eb8aa91ff6
bryansteele <bryan@steele101.com>
2020-12-27 16:42:48 -0500

    Completes 'The Basics' Exercises.

---
+++ intro_book/1_the_basics/3.rb
@@ -0,0 +1,17 @@
+movies = { :star_wars => 1378,
+           :stripes => 1945,
+           :big => 1863,
+           :little_tokyo => 2566,
+           :my_movie => 1523 }
+
+movies = { star_wars: 1378,
+           stripes: 1945,
+           big: 1863,
+           little_tokyo: 2566,
+           my_movie: 1523 }
+
+puts movies[:star_wars]
+puts movies[:stripes]
+puts movies[:big]
+puts movies[:little_tokyo]
+puts movies[:my_movie]
