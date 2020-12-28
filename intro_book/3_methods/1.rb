03fdbcf6de4094406b470d920517d00115a4c612
bryansteele <bryan@steele101.com>
2020-12-28 15:06:02 -0500

    Completes all Methods exercises.

---
+++ intro_book/3_methods/1.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+  "Hello " + name + "," + " Welcome!"
+end
+
+puts greeting("Bryan Steele")
---
+++ intro_book/3_methods/4.rb
@@ -0,0 +1,9 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+# Prints nothing to the screen, because of the explicit return, the last line that is executed.
