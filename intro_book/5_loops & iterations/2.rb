f9cb12f6f889a7df522f98bbbd28e3f8fb6e8a33
bryansteele <bryan@steele101.com>
2020-12-29 15:26:34 -0500

    Completes all Loops&Iteration Exercises.

---
+++ intro_book/5_loops & iterations/2.rb
@@ -0,0 +1,8 @@
+reply = ""
+
+while reply != "stop" do
+  puts "Enter STOP to exit."
+  reply = gets.chomp.downcase
+end
+
+puts "BYE BYE!"
---
+++ intro_book/5_loops & iterations/3.rb
@@ -0,0 +1,12 @@
+def countdown(start)
+  puts start
+  if start > 0
+    countdown(start - 1)
+  end
+end
+
+countdown(10)
+puts
+countdown(20)
+puts
+countdown(-3)
