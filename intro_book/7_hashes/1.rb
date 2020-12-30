351c4aedbf84364368d9654b4bca841c6a895738
bryansteele <bryan@steele101.com>
2020-12-30 13:55:42 -0500

    Hash Ex 1 Complete

---
+++ intro_book/7_hashes/1.rb
@@ -0,0 +1,13 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+
+fam = family.select do |k, v|
+  k == :sisters || k == :brothers
+end
+
+arr = fam.values.flatten
+
+p arr
