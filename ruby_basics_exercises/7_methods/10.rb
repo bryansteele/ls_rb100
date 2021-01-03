9c821dc74c237d055d49c888fcc19e2a782bbca9
bryansteele <bryan@steele101.com>
2021-01-03 16:41:00 -0500

    Methods Ex All Complete

--- ruby_basics_exercises/7_methods/10.rb
+++ ruby_basics_exercises/7_methods/10.rb
@@ -1,10 +1,16 @@
 names = ['Dave', 'Sally', 'George', 'Jessica']
 activities = ['walking', 'running', 'cycling']
 
+def name(names_arry)
+  names_arry.sample
+end
 
+def activity(activities_arry)
+  activities_arry.sample
+end
 
-
-
-
+def sentence(mth1, mth2)
+  "#{mth1} went #{mth2} today!"
+end
 
 puts sentence(name(names), activity(activities))
